2e9f66ad2fc850e052a6fde81799aaa30a292d5b
morkvd <markvdijken@gmail.com>
2016-03-22 22:52:58 +0100

    fixed time based trigger for cards
    cards now become visible when the triggerTime is reached

--- prototype_01.js
+++ prototype_01.js
@@ -1,43 +1,84 @@
 Cards = new Mongo.Collection("cards");
 
 if (Meteor.isClient) {
-  Session.set('uitzending', moment().format('2016-03-17 20:30', 'DD-MM-YYYY HH:mm'));
+  // uitzending 
+  Session.set('uitzending', moment().format('2016-03-23 20:30:00', 'DD-MM-YYYY HH:mm:ss Z'));
   Session.set('showFavs', false);
+  Session.set('cardType', null);
+
+  Meteor.startup(function () {
+    setInterval(function () {
+      Meteor.call("getServerTime", function (error, result) {
+        Session.set("time", result);
+      });
+    }, 1000);
+  });
 
   Template.registerHelper('formatDate', function(date) {
-    return moment(date).format('DD-MM-YYYY');
+    return moment(date).format('DD-MM-YYYY HH:mm:ss Z');
+  });
+
+  Template.header.helpers({
+    currentDate: function () {
+      return Session.get('uitzending');
+    }
+  });
+
+  Template.admin.helpers({
+    cardTypeIs: function (cardType) {
+      return Session.get('cardType') === cardType;
+    }
+  });
+
+  Template.admin.events({
+    "submit .new-card": function (event) {
+      event.preventDefault();
+      var text          = event.target.text.value,
+          title         = event.target.title.value,
+          type          = event.target.cardTypeOption.value,
+          triggerDate   = event.target.cardTriggerDate.value,
+          datePublished = Session.get('time');
+
+      console.log('input:', moment(triggerDate).unix());
+      console.log('input:', moment(Session.get('time')).unix());
+
+      Cards.insert({
+        title: title,
+        text: text,
+        datePublished: datePublished,
+        type: type,
+        triggerDate: triggerDate 
+      });
+
+      event.target.text.value = "";
+      event.target.title.value = "";
+      event.target.cardTriggerDate.value = "";
+    },
+    "click .card-type": function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      event.target.childNodes[1].checked = !event.target.childNodes[1].checked;
+      Session.set('cardType', event.target.childNodes[1].value);
+    }
   });
 
   Template.body.helpers({
     cards: function () {
       if (Session.get('showFavs')) {
-        return Cards.find({checked: {$ne: false}}, {sort: {datePublished: -1}});
+        return Cards.find({checked: {$ne: false}}, {sort: {datePublished: -1}}).filter;
       } else {
         return Cards.find({}, {sort: {datePublished: -1}});
       }
     },
     showFavs: function () {
       return Session.get('showFavs');
+    },
+    time: function () {
+      return Session.get("time");
     }
   });
 
   Template.body.events({
-    "submit .new-card": function (event) {
-      event.preventDefault();
-      var text = event.target.text.value,
-          title = event.target.title.value,
-          type = event.target.cardTypeOption.value;
-
-      Cards.insert({
-        title: title,
-        text: text,
-        datePublished: new Date(),
-        type: type
-      });
-
-      event.target.text.value = "";
-      event.target.title.value = "";
-    },
     "click .toggle-favs": function (event) {
       Session.set('showFavs', ! Session.get('showFavs'));
       event.target.checked = Session.get('showFavs'); 
@@ -45,9 +86,18 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.header.helpers({
-    currentDate: function () {
-      return Session.get('uitzending')
+  Template.card.helpers({
+    typeIs: function (type) {
+      console.log(this.type, type);
+      return this.type === type;
+    },
+    splitLines: function (text) {
+      return text.split(/\r?\n/);
+    },
+    released: function (time) {
+      console.log('release: ' + time, 'vs time: ' + Session.get('time'));
+      console.log();
+      return moment(time).isSameOrBefore(Session.get('time'));
     }
   });
 
@@ -68,7 +118,11 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.methods({
+    getServerTime: function () {
+      var _time = moment().format();
+      console.log(_time);
+      return _time;
+    }
   });
 }
---
+++ text.md
@@ -0,0 +1,38 @@
+Meer politie nodig tegen strijd drugssmokkel Zeeland
+
+
+De Zeeuwse havens zijn populair bij drugshandelaren. Een speciale Officier van justitie moet dit gaan aanpakken. En dat is volstrekt onvoldoende, zeggen zes Zeeuwse burgemeesters.
+
+Om de drugssmokkel aan te pakken, is volgens hen meer politie op het water nodig. De Zeeuwse havens zijn aantrekkelijk voor drugscriminelen. Er arriveren veel grote containerschepen uit Zuid-Amerika in de havens van Vlissingen en Terneuzen.
+
+Een jaar terug maakte EenVandaag deze reportage over de uit de klauwen lopende drugssmokkel in Zeeland:
+
+Kleine motorbootjes pikken de drugs op de uitgestrekte Zeeuwse wateren weer op. Alleen al dit jaar is er in Zeeland voor minstens 75 miljoen euro aan drugs gevonden. Om de problemen aan te pakken, is er half februari een speciale Officier van justitie aangesteld.
+
+In EenVandaag een gesprek met Jaap Gelok. De burgemeester van Borsele luidde samen met vijf andere burgemeesters de noodklok in een artikel in het NRC Handelsblad.
+
+Daarnaast Alexander van Dam, plaatsvervangend Hoofdofficier van Justitie Zeeland-West-Brabant.
+
+
+
+
+In het nieuws: Rusland trekt zicht terug uit Syrië
+
+
+Vanuit het niets leek het nieuws gisteravond ineens te komen. Rusland trekt zich terug uit Syrië. Oorlogsvliegtuigen en militairen reizen vanuit het Midden-Oosten terug naar Moskou.
+
+Over die opvallende Russische beslissing en welke doelen de Russen hebben bereikt, praten we in Radio EenVandaag met Ruslanddeskundige van onderzoeksbureau Most, Helga Salemon.
+
+Ook in de studio zit Thomas van Linge, bekend als ‘de kaartenmaker van Syrië’. Hij is altijd op de hoogte van de laatste stand van zaken en wie er waar in Syrië aan de macht is.
+
+Wat denkt u dat achter deze beslissing zit? We willen uw mening weten!
+
+
+
+
+Separatist Convoy Linked to MH17 Buk Transport
+
+
+At 10:40am on the morning of 17 July 2014, one of the first mentions of a Buk missile launcher in Donetsk reached the internet. This came in the form of a post on the “Donetsk is Ukraine!” VKontakte (VK) community, warning of the Buk’s arrival from Makeevka and the presence of three civilian vehicles:
+
+“Bad news. Around 9am, a hauler was going along the Makeevka highway from Makeevka in the direction of Donetsk. On the platform was a BukM1-M2? This AAMS proceeded to the intersection with Shakhtostroiteley Boulevard. The system was accompanied by a convoy that was composed of 1 gray Rav4 SUV, a camouflaged UAZ, and a dark blue Hyundai van with tinted windows. As of 9:15am, the vehicle was located at the intersection of Shakhtostroiteley and Ilycha. The militants got out of their cars, blocking 2 of the far left lanes. Obviously, they were waiting for logistical guidance.”
